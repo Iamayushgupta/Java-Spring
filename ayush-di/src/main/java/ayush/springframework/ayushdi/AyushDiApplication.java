f79ebb5e0c24dea8b1e461070206ae35b89ddea6
Ayush <lionelayush446@gmail.com>
2022-06-14 12:03:19 +0530

    Adding Profiles

--- ayush-di/src/main/java/ayush/springframework/ayushdi/AyushDiApplication.java
+++ ayush-di/src/main/java/ayush/springframework/ayushdi/AyushDiApplication.java
@@ -1,9 +1,6 @@
 package ayush.springframework.ayushdi;
 
-import ayush.springframework.ayushdi.controllers.ConstructorInjectedController;
-import ayush.springframework.ayushdi.controllers.MyController;
-import ayush.springframework.ayushdi.controllers.PropertyInjectedController;
-import ayush.springframework.ayushdi.controllers.SetterInjectedController;
+import ayush.springframework.ayushdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -29,6 +26,12 @@ public class AyushDiApplication {
 		System.out.println("----constructor------");
 		ConstructorInjectedController constructorInjectedController =(ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
+
+		//Checking for active profiles
+		System.out.println("--profiles--");
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayGreeting());
+
 
 	}
 }
